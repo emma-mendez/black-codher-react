2b40a06f952180e7f9fb48617592517cf1db5aff
ieseaacs <info@any1cansing.com>
2020-10-03 09:53:43 +0100

    This is my app first or second commit, unsure but here it is

---
+++ bookstore-react/src/Product.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Product () {
+    return ( 
+        <div>
+            <h2> Shoes</h2>
+            <p>16.99</p>
+            <p>something</p>
+        </div>
+    )
+}
+
+export default Product;
